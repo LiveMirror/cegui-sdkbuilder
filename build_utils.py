4c82758ba6c62498d769f8710fa04b873fd27fa8
Timotei Dolean <timotei21@gmail.com>
2014-10-04 19:57:11 +0300

    Enable parallel project builds

--- build_utils.py
+++ build_utils.py
@@ -101,7 +101,7 @@ def generateCEGUIDependenciesDirName(compiler):
 
 
 def generateMSBuildCommand(filename, configuration):
-    return ["msbuild", filename, "/p:Configuration=" + configuration, "/maxcpucount", "/verbosity:minimal"]
+    return ["msbuild", filename, "/p:Configuration=" + configuration, "/maxcpucount", "/m", "/verbosity:minimal"]
 
 
 def generateMingwMakeCommand(target=None):
